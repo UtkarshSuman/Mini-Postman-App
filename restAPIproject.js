b32cade3062523404afaf9a4804b1bd5a22602bc
UtkarshSuman <utkarshsuman7@gmail.com>
2025-11-02 20:14:31 +0530

    Fix render internal server error

--- restAPIproject.js
+++ restAPIproject.js
@@ -2,6 +2,11 @@ import express from "express";
 import bodyParser from "body-parser";
 import axios from "axios";
 import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const app = express();
 const port = process.env.PORT || 3000;
 
